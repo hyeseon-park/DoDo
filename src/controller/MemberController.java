ebf87c8264b280fa80315e271c08c2b6e072efdd
hyeseon-park <lightning350@naver.com>
2020-05-04 14:02:56 +0900

    로그인 & 회원가입 페이지 이동

--- src/controller/MemberController.java
+++ src/controller/MemberController.java
@@ -1,8 +1,25 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import service.MemberService;
 
 @Controller
+@RequestMapping("/member")
 public class MemberController {
+	@Autowired
+	MemberService memberService;
 
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String showLoginForm() {
+		return "/member/loginForm";
+	}
+
+	@RequestMapping(value = "/join", method = RequestMethod.GET)
+	public String showJoinForm() {
+		return "/member/joinForm";
+	}
 }
